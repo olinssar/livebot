1a57f8d6ff43abfe61cc933f4d80ba9d5b619182
Murugabotsown <119067136+Murugabotsown@users.noreply.github.com>
2023-05-26 05:16:11 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -30,7 +30,7 @@ UPSTREAM_REPO = getenv(
     "https://github.com/itz-star-boi/ShizukaXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/itz-star-boi/ShizukaXMusic")
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/https://github.com/Murugabotsown/ShizukaXMusic")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Star_X_Network")
