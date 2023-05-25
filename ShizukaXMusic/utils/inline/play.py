da120f60099388e44c413e5706cb9036cb05102f
Murugabotsown <119067136+Murugabotsown@users.noreply.github.com>
2023-05-26 05:07:11 +0530

    Update play.py

--- ShizukaXMusic/utils/inline/play.py
+++ ShizukaXMusic/utils/inline/play.py
@@ -6,7 +6,7 @@ import random
 
 
 selections = [
-    "▁▄▂▇▄▅▄▅▃",
+    "◉—————————",
     "▁▃▇▂▅▇▄▅▃",
     "▃▁▇▂▅▃▄▃▅",
     "▃▄▂▄▇▅▃▅▁",
@@ -40,7 +40,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 callback_data=f"add_playlist {videoid}",
             ),
             InlineKeyboardButton(
-                text="🌹 sᴏᴜʀᴄᴇ 🌹", url=f"https://github.com/itz-star-boi/ShizukaXMusic"
+                text="🌹 ᴏᴡɴᴇʀ 🌹", url=f"https://t.me/thavarajtj"
             ),
         ],
         [
@@ -68,7 +68,7 @@ def telegram_markup_timer(_, chat_id, played, dur):
                 text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
             ),
-            InlineKeyboardButton(text="🌹 sᴏᴜʀᴄᴇ 🌹", url=f"https://github.com/itz-star-boi/ShizukaXMusic"),
+            InlineKeyboardButton(text="🌹 ᴏᴡɴᴇʀ 🌹", url=f"https://github.com/t.me/thavarajtj"),
         ],
         [
             InlineKeyboardButton(
@@ -91,7 +91,7 @@ def stream_markup(_, videoid, chat_id):
                 text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
             ),
-            InlineKeyboardButton(text="🌹 sᴏᴜʀᴄᴇ 🌹", url=f"https://github.com/itz-star-boi/ShizukaXMusic"),
+            InlineKeyboardButton(text="🌹 ᴏᴡɴᴇʀ 🌹", url=f"https://github.com/t.me/thavarajtj"),
         ],
         [
             InlineKeyboardButton(
