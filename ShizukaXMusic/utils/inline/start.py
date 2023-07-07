fd56d22a7981c5c2a930c046e1da61d5084577aa
Megalalon <134177822+Megalalon@users.noreply.github.com>
2023-07-07 07:49:48 +0530

    Update start.py

--- ShizukaXMusic/utils/inline/start.py
+++ ShizukaXMusic/utils/inline/start.py
@@ -11,9 +11,7 @@ def start_pannel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_5"],
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-    
-       ]
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true"
     ]
     if SUPPORT_CHANNEL and SUPPORT_GROUP:
         buttons.append(
