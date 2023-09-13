718117f4909c91896d2351f81421a910a823857c
olinssar <143740876+olinssar@users.noreply.github.com>
2023-09-13 22:59:58 +0300

    Update start.py

--- ShizukaXMusic/utils/inline/start.py
+++ ShizukaXMusic/utils/inline/start.py
@@ -67,14 +67,14 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(text=_["S_B_6"], url=f"https://github.com/itz-star-boi/ShizukaXMusic"),
+                InlineKeyboardButton(text=_["S_B_6"], url=f"{SUPPORT_CHANNEL}"),
             ]
         )
     else:
         if GITHUB_REPO:
             buttons.append(
                 [
-                    InlineKeyboardButton(text=_["S_B_6"], url=f"https://github.com/itz-star-boi/ShizukaXMusic"),
+                    InlineKeyboardButton(text=_["S_B_6"], url=f"{SUPPORT_CHANNEL}"),
                 ]
             )
         if OWNER:
