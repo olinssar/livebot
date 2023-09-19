ff5d9bb2220058ddc65f19a06dc834e98911934a
olinssar <143740876+olinssar@users.noreply.github.com>
2023-09-19 20:00:09 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -6,59 +6,40 @@ from dotenv import load_dotenv
 from pyrogram import filters
 
 load_dotenv()
-
-API_ID = int(getenv("API_ID", "25710552"))
-API_HASH = getenv("API_HASH", "1f08de6bf83142a21f8601dd08d2a84c")
-
+API_ID = int(getenv("API_ID",""))
+API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
-
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "900"))
-
-SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "180"))
-
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID",""))
-
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME","⁪⁬ǫᴜʀᴀɴ ʟɪᴠᴇ")
-OWNER_ID = list(map(int, getenv("OWNER_ID","5422153027").split()))
-HEROKU_API_KEY = getenv("HEROKU_API_KEY") 
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT","900"))
+SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT","180"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID","-1001948876960"))
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME","بوت إذاعة القران")
+OWNER_ID = list(map(int, getenv("OWNER_ID","5422153027","5367442455","1786857536").split()))
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+BOT_ID = getenv("BOT_ID")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-
 UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO",
-    "https://github.com/olinssar/livebot",
-)
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/olinssar/livebot")
+"UPSTREAM_REPO","https://github.com/olinssar/livebot3",)
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH","main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
-
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NNINB")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/NKINB")
-
-
-AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "True")
-AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "5400"))
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL","https://t.me/NNINB")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP","https://t.me/NKINB")
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
+AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "11500"))
 AUTO_SUGGESTION_TIME = int(getenv("AUTO_SUGGESTION_TIME", "5400"))
 AUTO_DOWNLOADS_CLEAR = getenv("AUTO_DOWNLOADS_CLEAR", None)
-AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", True)
+AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", None)
 PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
 YOUTUBE_DOWNLOAD_EDIT_SLEEP = int(getenv("YOUTUBE_EDIT_SLEEP", "3"))
 TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
-
+GITHUB_REPO = getenv("GITHUB_REPO", None)
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
-
 VIDEO_STREAM_LIMIT = int(getenv("VIDEO_STREAM_LIMIT", "3"))
-
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "30"))
-
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "25"))
-
 CLEANMODE_DELETE_MINS = int(getenv("CLEANMODE_MINS", "5"))
-
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", "104857600"))
-
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "1073741824"))
 # https://www.gbmb.org/mb-to-bytes
 
@@ -78,52 +59,29 @@ chatstats = {}
 userstats = {}
 clean = {}
 autoclean = []
-
-
-START_IMG_URL = getenv("START_IMG_URL", "https://kindidev.com/live/photo_%D8%A7%D8%B5%D9%81%D8%B1.jpg")
-
-PING_IMG_URL = getenv(
-    "PING_IMG_URL",
-    "https://te.legra.ph/file/159d3f9e2d57dd02db970.jpg",
-)
-
+START_IMG_URL = getenv("START_IMG_URL","https://kindidev.com/live/photo_%D8%A7%D8%B5%D9%81%D8%B1.jpg")
+PING_IMG_URL = getenv("PING_IMG_URL","https://te.legra.ph/file/159d3f9e2d57dd02db970.jpg",)
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/6054be6dbfc0f654db62b.jpg"
-
 GLOBAL_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
-
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
-
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-
 YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-
 SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-
 SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-
 SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
-
-
 def time_to_seconds(time):
     stringt = str(time)
     return sum(
         int(x) * 60**i
         for i, x in enumerate(reversed(stringt.split(":")))
     )
-
-
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 SONG_DOWNLOAD_DURATION_LIMIT = int(
     time_to_seconds(f"{SONG_DOWNLOAD_DURATION}:00")
 )
-
-
 if UPSTREAM_REPO:
     if not re.match("(?:http|https)://", UPSTREAM_REPO):
         print(
