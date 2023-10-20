b5021bdba49cec6c732a8b3ac1cb7394215b0129
olinssar <143740876+olinssar@users.noreply.github.com>
2023-10-21 00:52:54 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,28 +1,41 @@
 import re
 import sys
 from os import getenv
+
 from dotenv import load_dotenv
 from pyrogram import filters
+
 load_dotenv()
-API_ID = int(getenv("API_ID",""))
+
+API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH")
+
 BOT_TOKEN = getenv("BOT_TOKEN")
+
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "900"))
+
 SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "180"))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001948876960"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "بوت إذاعة القران")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5422153027").split()))
-HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-BOT_ID = getenv("BOT_ID")
+
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
+
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+HEROKU_API_KEY = getenv("HEROKU_API_KEY") 
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/olinssar/livebot",
+    "https://github.com/its-star-boi/ShizukaXMusic",
+)
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/its-star-boi/ShizukaXMusic")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TH3NK")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/NKINB")
+
+
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "11500"))
 AUTO_SUGGESTION_TIME = int(getenv("AUTO_SUGGESTION_TIME", "5400"))
@@ -40,7 +53,6 @@ PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "25"))
 CLEANMODE_DELETE_MINS = int(getenv("CLEANMODE_MINS", "5"))
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", "104857600"))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "1073741824"))
-# https://www.gbmb.org/mb-to-bytes
 
 STRING1 = getenv("STRING_SESSION", None)
 STRING2 = getenv("STRING_SESSION2", None)
@@ -58,29 +70,52 @@ chatstats = {}
 userstats = {}
 clean = {}
 autoclean = []
-START_IMG_URL = getenv("START_IMG_URL","https://kindidev.com/live/photo_quran.jpg")
-PING_IMG_URL = getenv("PING_IMG_URL","https://te.legra.ph/file/159d3f9e2d57dd02db970.jpg",)
+
+
+START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/f576595d249f645c886fc.jpg")
+
+PING_IMG_URL = getenv(
+    "PING_IMG_URL",
+    "https://te.legra.ph/file/159d3f9e2d57dd02db970.jpg",
+)
+
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/6054be6dbfc0f654db62b.jpg"
+
 GLOBAL_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
+
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
+
 YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+
 SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
+
 SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
+
 SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+
+
 def time_to_seconds(time):
     stringt = str(time)
     return sum(
         int(x) * 60**i
         for i, x in enumerate(reversed(stringt.split(":")))
     )
+
+
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 SONG_DOWNLOAD_DURATION_LIMIT = int(
     time_to_seconds(f"{SONG_DOWNLOAD_DURATION}:00")
 )
+
+
 if UPSTREAM_REPO:
     if not re.match("(?:http|https)://", UPSTREAM_REPO):
         print(
@@ -91,9 +126,9 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://kindidev.com/live/photo_quran.jpg"
+            PING_IMG_URL = "https://telegra.ph/file/56d1760224589ee370186.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://kindidev.com/live/photo_quran.jpg"
+            START_IMG_URL = "https://te.legra.ph/file/56d1760224589ee370186.jpg"
